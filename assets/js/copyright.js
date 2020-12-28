fa342e26a21f8bfadb5e6622037d0cdeab0f6fc9
ououn <33188248+ououn@users.noreply.github.com>
2020-12-27 17:55:59 -1000

    Add files via upload

--- assets/js/copyright.js
+++ assets/js/copyright.js
@@ -1,19 +1,10 @@
 document.write('\
-\
-    <div id="copyright">\
-\
-    <ul><li>&copy; 2020</li>\
-\
-    <li>Website Design: <a href="https://html5up.net">HTML5 UP</a></li>\
-\
-    <li>Lightbox: <a href="https://www.frescojs.com/">Frescojs</a></li>\
-\
-    <li><a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">\
-\
-    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/3.0/80x15.png" /></a></li>\
-\
-    </ul>\
-\
-    </div>\
+\<div id="copyright">\
+\   <ul><li>&copy; 2020 OuOun</li>\
+\       <li>Website Template: <a target="_blank" rel="noopener noreferer" href="https://html5up.net">HTML5 UP</a></li>\
+\       <li><a target="_blank" rel="noopener noreferer licencse" href="http://creativecommons.org/licenses/by-nc/3.0/">\
+\       <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/3.0/80x15.png" /></a></li>\
+\   </ul>\
+\</div>\
 \
 ');
